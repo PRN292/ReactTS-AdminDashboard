a46012bdbdfe7a3fa7692af45dff951ca30f9589
bangmaple <webcam1362000@gmail.com>
2021-01-05 14:38:04 +0700

    first commit and menu component

---
+++ src/components/dish.ts
@@ -0,0 +1,9 @@
+export default class Dish {
+    id: number | undefined;
+    name: string | undefined;
+    image: string | undefined;
+    category: string | undefined;
+    label: string | undefined;
+    price: string | undefined;
+    description: string | undefined;
+}
